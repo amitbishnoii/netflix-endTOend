b7a13e1d16e144bdebddbe82b1d00479eacc7c5e
amitbishnoii <amitbishnoi29012@gmail.com>
2026-08-01 15:54:43 +0530

    practice basic SSR

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
 import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
 
-export const app = express();
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("hello world");
+    res.render("login");
 });
+
+app.get("/login", (req, res) => {
+    // res.render("login");
+    if (req.query.username = "admin") {
+        res.send("admin page");
+    } else {
+        res.send("hello world");
+    }
+});
+
+export default app;
