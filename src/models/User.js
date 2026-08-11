e12086814b4bd00df2f487824f3f8f26cbe7506e
amitbishnoii <amitbishnoi29012@gmail.com>
2026-08-11 23:58:10 +0530

    practice schema validation

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,22 @@
 import mongoose from "mongoose";
 
 const userModel = new mongoose.Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-    email: { type: String, required: true },
-    age: { type: Number, required: true },
+    username: { type: String, required: true, trim: true },
+    password: {
+        type: String,
+        required: true,
+        min: [8, "minimum 8 characters are required"],
+        max: [55, "limit exceeded"],
+        trim: true,
+    },
+    email: { type: String, required: true, trim: true },
+    age: {
+        type: Number,
+        required: true,
+        min: [12, "min age is 12 years"],
+        max: [100, "max age is 100"],
+    },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
 });
 
 const User = mongoose.model("User", userModel);
