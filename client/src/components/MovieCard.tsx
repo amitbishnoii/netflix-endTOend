f3952da0dd131bd8f82dfff367c0464df5c54484
amitbishnoii <amitbishnoi29012@gmail.com>
2026-08-26 10:12:11 +0530

    create movie card

--- client/src/components/MovieCard.tsx
+++ client/src/components/MovieCard.tsx
@@ -10,16 +10,29 @@ const MovieCard = ({
     return (
         <div
             className="group relative rounded-xl overflow-hidden bg-zinc-900 border border-white/10 
-                        aspect-2/3 cursor-pointer transition-transform duration-300 hover:-translate-y-1"
+                        aspect-2/3 transition-transform duration-300 hover:-translate-y-1 cursor-pointer"
         >
             <img
                 src={imgUrl}
                 alt={movieTitle}
                 loading="lazy"
-                className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
+                className="w-full h-full object-cover transition-transform duration-200 group-hover:scale-105"
             />
 
             <div className="absolute inset-0 bg-linear-to-t from-black via-black/20 to-transparent" />
+            <div
+                className="hidden group-hover:flex absolute inset-0 items-center justify-center text-center
+                bg-[radial-gradient(ellipse_at_center,rgba(0,0,0,0.3)_0%,rgba(0,0,0,0.85)_100%)]
+                backdrop-blur-[1px] transition-opacity duration-300"
+            >
+                <button
+                    className="text-white text-xs sm:text-sm font-medium tracking-wide uppercase
+                     border border-white/20 rounded-full px-4 py-1.5
+                     bg-white/5 backdrop-blur-sm"
+                >
+                    Click to watch
+                </button>
+            </div>
 
             <div className="absolute bottom-0 left-0 right-0 p-3">
                 <h3 className="text-white text-sm font-semibold leading-tight line-clamp-2">
